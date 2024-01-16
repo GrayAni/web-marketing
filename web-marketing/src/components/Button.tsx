53bc6d59626d47282926edcdfc93216b3b8a5335
Chinyere Ani <chinyereani1@gmail.com>
2024-01-16 12:36:11 +0100

    New project with button component setup

---
+++ web-marketing/src/components/Button.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from "react";
+
+interface ButtonProps {
+  // Add any props you might need for the button
+}
+
+const Button: FC<ButtonProps> = () => {
+  return <div>This is the web marketing button</div>;
+};
+
+export default Button;
